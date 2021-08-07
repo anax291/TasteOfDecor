1b72fecf760e81a0f4bba6e02dcc8d0bc1245b8b
Siddiq <siddiqahmed882@gmail.com>
2021-08-07 16:43:27 +0500

    customization

---
+++ public/js/customization.js
@@ -0,0 +1,34 @@
+const products = document.querySelectorAll('.prod');
+products.forEach((prod) => dragElement(prod));
+
+function dragElement(terrariumElement) {
+  let pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  terrariumElement.onpointerdown = pointerDrag;
+
+  function pointerDrag(e) {
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    document.onpointermove = elementDrag;
+    document.onpointerup = stopElementDrag;
+  }
+
+  function elementDrag(e) {
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    terrariumElement.style.top = terrariumElement.offsetTop - pos2 + 'px';
+    terrariumElement.style.left = terrariumElement.offsetLeft - pos1 + 'px';
+  }
+
+  function stopElementDrag() {
+    document.onpointerup = null;
+    document.onpointermove = null;
+  }
+}
