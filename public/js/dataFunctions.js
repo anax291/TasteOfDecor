2b2a1f5fff99ce67abde759a19f8122e93b3c440
Siddiq <siddiqahmed882@gmail.com>
2021-08-07 20:13:37 +0500

    layout fixes

--- public/js/dataFunctions.js
+++ public/js/dataFunctions.js
@@ -65,7 +65,7 @@ export const injectProducts = (cardTemplate, products, productContainer) => {
   });
 };
 
-// Email validation
+/* Email validation */
 export const validateEmail = (email) => {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
