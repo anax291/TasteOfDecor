0a5dd79cde9e3b8f5bc573da818a54ba7623566f
Siddiq <siddiqahmed882@gmail.com>
2021-11-23 20:47:33 +0500

    admin updates

--- public/admin/js/productView.js
+++ public/admin/js/productView.js
@@ -18,16 +18,14 @@ const displayPage = (product) => {
 };
 
 const createProductView = (product) => {
-  const productView = document.createElement('section');
-  productView.classList.add('product__view');
+  const productView = createElementWithOptions('section', ['product__view']);
   productView.appendChild(createImageGrid(product));
   productView.appendChild(createProductDetails(product));
   return productView;
 };
 
 const createImageGrid = ({ name, imgSrc: images }) => {
-  const imageGrid = document.createElement('div');
-  imageGrid.classList.add('image-grid');
+  const imageGrid = createElementWithOptions('div', ['image-grid']);
   imageGrid.appendChild(createImage(images[0], name, 'primary__image'));
   const imgs = images.map((image) => createImage(image, name));
   imgs.forEach((img) => imageGrid.appendChild(img));
@@ -52,8 +50,7 @@ const togglePrimaryImage = (e) => {
 };
 
 const createProductDetails = (product) => {
-  const productDetails = document.createElement('div');
-  productDetails.classList.add('product__details', 'flow');
+  const productDetails = createElementWithOptions('div', ['product__details', 'flow']);
   productDetails.appendChild(createHeader(product.name, product.price));
   productDetails.appendChild(displayProductRatings(product.reviews));
   productDetails.appendChild(displayProductDescription(product.description));
@@ -63,12 +60,12 @@ const createProductDetails = (product) => {
 
 const createHeader = (name, price) => {
   const fragment = document.createDocumentFragment();
-  const productName = document.createElement('h2');
-  productName.classList.add('product__name');
-  productName.textContent = name;
-  const productPrice = document.createElement('p');
-  productPrice.classList.add('product__price');
-  productPrice.textContent = `Rs. ${numberWithCommas(price)}`;
+  const productName = createElementWithOptions('h2', ['product__name'], name);
+  const productPrice = createElementWithOptions(
+    'p',
+    ['product__price'],
+    `Rs. ${numberWithCommas(price)}`
+  );
   fragment.appendChild(productName);
   fragment.appendChild(productPrice);
   return fragment;
@@ -104,35 +101,28 @@ const getRatingsInfo = (reviews) => {
 
 const displayProductDescription = (description) => {
   if (description.length === 1) {
-    const p = document.createElement('p');
-    p.classList.add('product__description');
-    p.textContent = [description];
+    const p = createElementWithOptions('p', ['product__description'], [description]);
     return p;
   }
-  const ul = document.createElement('ul');
-  ul.classList.add('product__description');
+  const ul = createElementWithOptions('ul', ['product__description']);
   const lis = description.map((desc) => createDescriptionItem(desc));
   lis.forEach((li) => ul.appendChild(li));
   return ul;
 };
 
 const createDescriptionItem = (desc) => {
-  const li = document.createElement('li');
-  li.textContent = desc;
+  const li = createElementWithOptions('li', undefined, desc);
   return li;
 };
 
 const createEditBtn = (id) => {
-  const btn = document.createElement('a');
+  const btn = createElementWithOptions('a', ['btn', 'edit-product'], 'Edit Product...');
   btn.href = `./editProduct.html?id=${id}`;
-  btn.classList.add('btn', 'edit-product');
-  btn.textContent = 'Edit Product...';
   return btn;
 };
 
 const createReviewsView = (reviews) => {
-  const reviewsSection = document.createElement('section');
-  reviewsSection.classList.add('reviews', 'container');
+  const reviewsSection = createElementWithOptions('section', ['reviews', 'container']);
   const title = createElementWithOptions('h2', ['review-title'], 'Reviews');
   reviewsSection.appendChild(title);
   if (reviews.length) {
@@ -146,8 +136,7 @@ const createReviewsView = (reviews) => {
 };
 
 const createReviewItem = (review, reviews) => {
-  const reviewItem = document.createElement('article');
-  reviewItem.classList.add('review', 'flow');
+  const reviewItem = createElementWithOptions('artice', ['review', 'flow']);
   reviewItem.appendChild(createDeleteBtn(review.id, reviews));
   reviewItem.appendChild(displayMetaInfo(review));
   reviewItem.appendChild(addRatings(Number(review.ratings)));
@@ -156,8 +145,12 @@ const createReviewItem = (review, reviews) => {
 };
 
 const createDeleteBtn = (id, reviews) => {
-  const deleteBtn = document.createElement('button');
-  deleteBtn.classList.add('delete-review', 'far', 'fa-trash-alt', 'fa-2x');
+  const deleteBtn = createElementWithOptions('button', [
+    'delete-review',
+    'far',
+    'fa-trash-alt',
+    'fa-2x',
+  ]);
   deleteBtn.ariaLabel = 'Delete this review';
   deleteBtn.title = 'Delete this review';
   deleteBtn.addEventListener('click', () => deleteReview(id, reviews));
